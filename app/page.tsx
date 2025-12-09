c3cbd6910c98970d9745b806dec2d5c691e441f0
DillonNicholas <nicholasdillon1911@gmail.com>
2025-12-10 01:38:14 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,49 @@
+"use client";
 import Image from "next/image";
+import Footer from "@/components/footer";
+import { ImRocket } from "react-icons/im";
+import { ImHangouts } from "react-icons/im";
+import { ImSphere } from "react-icons/im";
+import { ImUserCheck } from "react-icons/im";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="container">
+      <div className="image-logo">
+        <Image src="/logo.png" width={2400} height={200} alt="a logo" />
+      </div>
+      <div className="content">
+        <h1>Welcome toABCDelivery_Import_Export SDN BHD.</h1>
+        <p>Your most trusted partner in shipping to the Moon.</p>
+      </div>
+      <div className="box-container">
+        <div className="align">
+          <ImRocket />
+          <p className="text">Fast Delivery</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="align">
+          <ImHangouts />
+          <p className="text">24/7 Support</p>
         </div>
-      </main>
+        <div className="align">
+          <ImSphere />
+          <p className="text">Global Coverage</p>
+        </div>
+        <div className="align">
+          <ImUserCheck />
+          <p className="text">Trusted Service</p>
+        </div>
+      </div>
+      <div className="message">Follow Us!</div>
+      <div className="button-section">
+        <button className="button" onClick={handleClick}>
+          Start Shipping Now!
+        </button>
+      </div>
+      <Footer /> {}
     </div>
   );
+  function handleClick() {
+    alert("Button clicked!");
+  }
 }
---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import styles from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className={styles.footer}>
+        <p>© 2024 ABCDelivery_Import_Export SDN BHD. All rights reserved.</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
